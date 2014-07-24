22ca4e908c05bb5850bbb1990c2ad86e4fe5d709
James Cazzetta <james.cazzetta@gmail.com>
2014-07-24 10:03:07 +0200

    Did some minor changes, fixed docblocks and replaced a deprecated function.

--- src/Plugin/views/field/Link.php
+++ src/Plugin/views/field/Link.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Definition of \Drupal\media_entity\Plugin\views\field\Link.
+ * Contains \Drupal\media_entity\Plugin\views\field\Link.
  */
 
 namespace Drupal\media_entity\Plugin\views\field;
@@ -11,7 +11,7 @@ use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
 
 /**
- * Field handler to present a link to the node.
+ * Field handler to present a link to the media item.
  *
  * @ingroup views_field_handlers
  *
@@ -26,12 +26,18 @@ class Link extends FieldPluginBase {
     return FALSE;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   protected function defineOptions() {
     $options = parent::defineOptions();
     $options['text'] = array('default' => '', 'translatable' => TRUE);
     return $options;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function buildOptionsForm(&$form, &$form_state) {
     $form['text'] = array(
       '#type' => 'textfield',
@@ -62,20 +68,20 @@ class Link extends FieldPluginBase {
   }
 
   /**
-   * Prepares the link to the node.
+   * Prepares the link to the media.
    *
-   * @param \Drupal\Core\Entity\EntityInterface $node
-   *   The node entity this field belongs to.
+   * @param \Drupal\Core\Entity\EntityInterface $media
+   *   The media entity this field belongs to.
    * @param ResultRow $values
    *   The values retrieved from the view's result set.
    *
    * @return string
    *   Returns a string for the link text.
    */
-  protected function renderLink($node, ResultRow $values) {
-    if ($node->access('view')) {
+  protected function renderLink($media, ResultRow $values) {
+    if ($media->access('view')) {
       $this->options['alter']['make_link'] = TRUE;
-      $this->options['alter']['path'] = 'media/' . $node->id();
+      $this->options['alter']['path'] = 'media/' . $media->id();
       $text = !empty($this->options['text']) ? $this->options['text'] : t('View');
       return $text;
     }
--- src/Plugin/views/field/LinkDelete.php
+++ src/Plugin/views/field/LinkDelete.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Definition of Drupal\media_entity\Plugin\views\field\LinkDelete.
+ * Contains Drupal\media_entity\Plugin\views\field\LinkDelete.
  */
 
 namespace Drupal\media_entity\Plugin\views\field;
@@ -10,7 +10,7 @@ namespace Drupal\media_entity\Plugin\views\field;
 use Drupal\views\ResultRow;
 
 /**
- * Field handler to present a link to delete a node.
+ * Field handler to present a link to delete the media item.
  *
  * @ingroup views_field_handlers
  *
@@ -19,24 +19,24 @@ use Drupal\views\ResultRow;
 class LinkDelete extends Link {
 
   /**
-   * Prepares the link to delete a node.
+   * Prepares the link to delete the media item.
    *
-   * @param \Drupal\Core\Entity\EntityInterface $node
-   *   The node entity this field belongs to.
+   * @param \Drupal\Core\Entity\EntityInterface $media
+   *   The media entity this field belongs to.
    * @param \Drupal\views\ResultRow $values
    *   The values retrieved from the view's result set.
    *
    * @return string
    *   Returns a string for the link text.
    */
-  protected function renderLink($node, ResultRow $values) {
-    // Ensure user has access to delete this node.
-    if (!$node->access('delete')) {
+  protected function renderLink($media, ResultRow $values) {
+    // Ensure user has access to delete this media item.
+    if (!$media->access('delete')) {
       return;
     }
 
     $this->options['alter']['make_link'] = TRUE;
-    $this->options['alter']['path'] = "media/" . $node->id() . "/delete";
+    $this->options['alter']['path'] = "media/" . $media->id() . "/delete";
     $this->options['alter']['query'] = drupal_get_destination();
 
     $text = !empty($this->options['text']) ? $this->options['text'] : t('Delete');
